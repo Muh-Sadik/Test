4eab41f177e0a67081e6ad8062bf363242fa9e1a
Muh-Sadik <157655580+Muh-Sadik@users.noreply.github.com>
2024-03-06 20:02:53 +0000

    trial

---
+++ CMakeFiles/CONTACT_DETECTOR.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/muhammed/Documents/racketrythym/src/tests/lab_mictests/CONTACT_DETECTOR.cpp" "CMakeFiles/CONTACT_DETECTOR.dir/CONTACT_DETECTOR.cpp.o" "gcc" "CMakeFiles/CONTACT_DETECTOR.dir/CONTACT_DETECTOR.cpp.o.d"
+  "/home/muhammed/Documents/racketrythym/src/tests/lab_mictests/main.cpp" "CMakeFiles/CONTACT_DETECTOR.dir/main.cpp.o" "gcc" "CMakeFiles/CONTACT_DETECTOR.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ D/build/CMakeFiles/CONTACT_DETECTOR.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CONTACT_DETECTOR.dir/CONTACT_DETECTOR.cpp.o"
+  "CMakeFiles/CONTACT_DETECTOR.dir/CONTACT_DETECTOR.cpp.o.d"
+  "CMakeFiles/CONTACT_DETECTOR.dir/main.cpp.o"
+  "CMakeFiles/CONTACT_DETECTOR.dir/main.cpp.o.d"
+  "CONTACT_DETECTOR"
+  "CONTACT_DETECTOR.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CONTACT_DETECTOR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
